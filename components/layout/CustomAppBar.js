9884c751b3050e86f0e47986c60f2c51032302d1
Carlos Á. Vereterra <cvereterra@gmail.com>
2020-10-03 20:01:52 +0200

    Make separate styling for custom appbar

--- components/layout/CustomAppBar.js
+++ components/layout/CustomAppBar.js
@@ -2,9 +2,50 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-const CustomAppBar = ({ classes, handleDrawerToggle }) => {
+import Typography from "@material-ui/core/Typography";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: (props) => `calc(100% - ${props.drawerWidth}px)`,
+      marginLeft: 0,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const CustomAppBar = ({ handleDrawerToggle, ...props }) => {
+  const classes = useStyles(props);
+  const auth = false;
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
@@ -17,9 +58,45 @@ const CustomAppBar = ({ classes, handleDrawerToggle }) => {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap>
+        <Typography variant="h6" noWrap className={classes.title}>
           My app
         </Typography>
+        {auth && (
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
+        )}
+        {!auth && (
+          <div>
+            <Button color="inherit">Login</Button>
+          </div>
+        )}
       </Toolbar>
     </AppBar>
   );
--- components/layout/ResponsiveDrawer.js
+++ components/layout/ResponsiveDrawer.js
@@ -16,18 +16,7 @@ const useStyles = makeStyles((theme) => ({
       flexShrink: 0,
     },
   },
-  appBar: {
-    [theme.breakpoints.up("sm")]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("sm")]: {
-      display: "none",
-    },
-  },
+
   // necessary for content to be below app bar
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
@@ -51,7 +40,7 @@ function ResponsiveDrawer({ children }) {
     <div className={classes.root}>
       <CssBaseline />
       <CustomAppBar
-        classes={classes}
+        drawerWidth={drawerWidth}
         handleDrawerToggle={handleDrawerToggle}
       ></CustomAppBar>
       <CustomDrawer
