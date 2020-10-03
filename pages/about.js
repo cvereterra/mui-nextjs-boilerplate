5c60a6cf641e427f20146198f1912b9827bb3bd2
Carlos Á. Vereterra <cvereterra@gmail.com>
2020-10-03 17:38:23 +0200

    Move ResponsiveDrawer to _app.js

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import ResponsiveDrawer from "@/ui/ResponsiveDrawer";
 
 export default function About() {
-  return (
-    <ResponsiveDrawer>
-      <Typography>About page!</Typography>
-    </ResponsiveDrawer>
-  );
+  return <Typography>About page!</Typography>;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import ResponsiveDrawer from "@/ui/ResponsiveDrawer";
 
 export default function Index() {
-  return (
-    <ResponsiveDrawer>
-      <Typography>Index. Content goes here!</Typography>
-    </ResponsiveDrawer>
-  );
+  return <Typography>Index. Content goes here!</Typography>;
 }
